ebfdaba105c7d8e8a0319c00c9517d490ace0d5c
JM <sallaojohnmark2772@gmail.com>
2024-10-03 23:48:38 +0800

    login done

--- header.php
+++ header.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="login.css" />
     <link rel="stylesheet" href="css\bootstrap.min.css" />
     <script src="js\bootstrap.bundle.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
--- login.php
+++ login.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login & Sign Up</title>
-    <link rel="stylesheet" href="login1.css" />
+    <link rel="stylesheet" href="login3.css" />
     <link rel="stylesheet" href="css\bootstrap.min.css" />
     <script src="js\bootstrap.bundle.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -13,77 +13,79 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
+
+
 </head>
 
 <body>
-
-    <div class="container m-auto">
-        <!-- Login Form -->
-        <form action="logconn.php" method="post" class="login-form">
-            <h1>Login</h1>
-            <div class="form-group">
-                <input type="text" placeholder="Username" name="uname" required>
-            </div>
-            <div class="form-group">
-                <input type="password" placeholder="Password" name="password" required>
-            </div>
-            <div class="form-group">
-                <button type="submit" name="login"> Login</button>
-            </div>
-        </form>
-        <!-- Sign Up Button at the Top -->
-        <button class="btn-signup" type="button" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop"
-            aria-controls="staticBackdrop">
-            Sign up here!
-        </button>
-
-    </div>
-
-    <!-- Offcanvas for Sign Up -->
-    <div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop"
-        aria-labelledby="staticBackdropLabel">
-        <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="staticBackdropLabel">Sign Up</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    <div class="main_container">
+        <div class="container m-auto">
+            <!-- Login Form -->
+            <form action="logconn.php" method="post" class="login-form">
+                <h1>Login</h1>
+                <div class="form-group">
+                    <input type="text" placeholder="Username" name="uname" required>
+                </div>
+                <div class="form-group">
+                    <input type="password" placeholder="Password" name="password" required>
+                </div>
+                <div class="form-group">
+                    <button type="submit" name="login"> Login</button>
+                </div>
+                <h5>or</h5>
+                <button class="btn-signup" type="button" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop"
+                    aria-controls="staticBackdrop">
+                    Sign up here!
+                </button>
+            </form>
         </div>
-        <div class="offcanvas-body">
-            <form action="regconn.php" method="post">
-                <div class="container">
-                    <h1>Sign Up</h1>
-                    <div class="row">
-                        <div class="col-md-4">
-                            <div class="form-group">
-                                <input type="text" name="fname" placeholder="First Name" />
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div>
+
+        <!-- Offcanvas for Sign Up -->
+        <div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop"
+            aria-labelledby="staticBackdropLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="staticBackdropLabel">Sign Up</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <form action="regconn.php" method="post">
+                    <div class="container">
+                        <h1>Sign Up</h1>
+                        <div class="row">
+                            <div class="col-md-5">
                                 <div class="form-group">
-                                    <input type="text" name="lname" placeholder="Last Name" />
+                                    <input type="text" name="fname" placeholder="First Name" />
+                                </div>
+                            </div>
+                            <div class="col-md-7">
+                                <div>
+                                    <div class="form-group">
+                                        <input type="text" name="lname" placeholder="Last Name" />
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                        <div class="form-group">
+                            <input type="email" name="email" placeholder="Email" />
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="uname" placeholder="User Name" />
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="address" placeholder="Full Address" />
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="password" placeholder="Password" />
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="confirm" placeholder="Confirm Password" />
+                        </div>
+                        <div class="form-group">
+                            <button type="submit">Submit</button>
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <input type="email" name="email" placeholder="Email" />
-                    </div>
-                    <div class="form-group">
-                        <input type="text" name="uname" placeholder="User Name" />
-                    </div>
-                    <div class="form-group">
-                        <input type="text" name="address" placeholder="Full Address" />
-                    </div>
-                    <div class="form-group">
-                        <input type="password" name="password" placeholder="Password" />
-                    </div>
-                    <div class="form-group">
-                        <input type="password" name="confirm" placeholder="Confirm Password" />
-                    </div>
-                    <div class="form-group">
-                        <button type="submit">Submit</button>
-                    </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     </div>
 
